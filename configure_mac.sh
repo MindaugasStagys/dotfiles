fea30680346bf2b045420804f5b7e866d95ee217
Mindaugas Stagys <mindaugasstagys@stokex.com>
2022-10-20 15:53:54 +0300

    curl

--- configure_mac.sh
+++ configure_mac.sh
@@ -6,8 +6,9 @@ cd "$parent_path"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Oh-my-zsh and plugins
-curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
+curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+bash install.sh
+rm install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
