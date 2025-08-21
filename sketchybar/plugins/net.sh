c66a84a8527c84ba457e349794924afca208af80
MindaugasStagys <m.stagys@gmail.com>
2025-08-21 12:21:58 +0300

    net.sh

--- sketchybar/plugins/net.sh
+++ sketchybar/plugins/net.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-LABEL=$(system_profiler SPAirPortDataType | awk '/Current Network/ {getline;$1=$1; gsub(":",""); print;exit}')
+LABEL=$(system_profiler SPAirPortDataType -detailLevel basic | awk '/Current Network/ {getline;$1=$1;print $0 | "tr -d ':'";exit}')
 
 sketchybar --set $NAME label=$LABEL
